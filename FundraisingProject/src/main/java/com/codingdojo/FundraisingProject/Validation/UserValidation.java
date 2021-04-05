cc67688d6d1275ea06b01466bf37c201f0127075
zking63 <zoemking63@gmail.com>
2021-04-05 09:19:40 -0400

    validation added to validator

--- FundraisingProject/src/main/java/com/codingdojo/FundraisingProject/Validation/UserValidation.java
+++ FundraisingProject/src/main/java/com/codingdojo/FundraisingProject/Validation/UserValidation.java
@@ -2,6 +2,7 @@ package com.codingdojo.FundraisingProject.Validation;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
 
 import com.codingdojo.FundraisingProject.Models.User;
@@ -15,5 +16,19 @@ public class UserValidation implements Validator{
 	@Override
 	public boolean supports(Class<?> clazz) {
 		return User.class.equals(clazz);
+	}
+	
+	@Override
+	public void validate(Object target, Errors errors) {
+        User user = (User) target;
+        
+        if(this.urepo.findByEmail(user.getEmail()) != null) {
+        	errors.rejectValue("email", "Unique");
+        }
+        
+        if (!user.getPasswordConfirmation().equals(user.getPassword())) {
+            errors.rejectValue("passwordConfirmation", "Match");
+        } 
+		
 	}
 }
