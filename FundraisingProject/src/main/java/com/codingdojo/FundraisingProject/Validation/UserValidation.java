0b4f7b865d38800ce7640ce403061df0e163f4cd
zking63 <zoemking63@gmail.com>
2021-04-05 09:17:30 -0400

    validator created

---
+++ FundraisingProject/src/main/java/com/codingdojo/FundraisingProject/Validation/UserValidation.java
@@ -0,0 +1,9 @@
+package com.codingdojo.FundraisingProject.Validation;
+
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Validator;
+
+@Component
+public class UserValidation implements Validator{
+
+}
