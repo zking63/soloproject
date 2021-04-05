319bd04446422fc67d3dfa7e7c1d3d10ee51ca94
zking63 <zoemking63@gmail.com>
2021-04-05 14:02:48 -0400

    donor service completed

--- FundraisingProject/src/main/java/com/codingdojo/FundraisingProject/Services/DonorService.java
+++ FundraisingProject/src/main/java/com/codingdojo/FundraisingProject/Services/DonorService.java
@@ -1,12 +1,26 @@
 package com.codingdojo.FundraisingProject.Services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.codingdojo.FundraisingProject.Models.Donor;
 import com.codingdojo.FundraisingProject.Repositories.DonorRepo;
+
 
 @Service
 public class DonorService {
 	@Autowired
 	private DonorRepo drepo;
+	
+	public Donor createDonor(Donor donor) {
+		return drepo.save(donor);
+	}
+	public List<Donor> allDonors() {
+		return drepo.findAll();
+	}
+	public Donor findbyId(Long id) {
+		return drepo.findById(id).orElse(null);
+	}
 }
