a8f6f3642906984f631efa3f993f46dd7d47cc80
zking63 <zoemking63@gmail.com>
2021-04-05 09:19:01 -0400

    override added to validator

--- FundraisingProject/src/main/java/com/codingdojo/FundraisingProject/Validation/UserValidation.java
+++ FundraisingProject/src/main/java/com/codingdojo/FundraisingProject/Validation/UserValidation.java
@@ -1,9 +1,19 @@
 package com.codingdojo.FundraisingProject.Validation;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.Validator;
 
+import com.codingdojo.FundraisingProject.Models.User;
+import com.codingdojo.FundraisingProject.Repositories.UserRepo;
+
 @Component
 public class UserValidation implements Validator{
-
+	@Autowired
+	private UserRepo urepo;
+	
+	@Override
+	public boolean supports(Class<?> clazz) {
+		return User.class.equals(clazz);
+	}
 }
