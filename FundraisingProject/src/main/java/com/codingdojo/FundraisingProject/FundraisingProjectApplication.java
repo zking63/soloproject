abd07294541523b83fa7701eec0d5042133e894c
zking63 <zoemking63@gmail.com>
2021-04-05 08:50:39 -0400

    project created

---
+++ FundraisingProject/src/main/java/com/codingdojo/FundraisingProject/FundraisingProjectApplication.java
@@ -0,0 +1,13 @@
+package com.codingdojo.FundraisingProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundraisingProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FundraisingProjectApplication.class, args);
+	}
+
+}
---
+++ FundraisingProject/src/test/java/com/codingdojo/FundraisingProject/FundraisingProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingdojo.FundraisingProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundraisingProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
