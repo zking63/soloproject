b6eeb85eb91159b2f5e13c9e7ab0c4ac872a3cc2
zking63 <zoemking63@gmail.com>
2021-04-05 13:58:35 -0400

    find all added to donor repo

--- FundraisingProject/src/main/java/com/codingdojo/FundraisingProject/Repositories/DonorRepo.java
+++ FundraisingProject/src/main/java/com/codingdojo/FundraisingProject/Repositories/DonorRepo.java
@@ -1,9 +1,13 @@
 package com.codingdojo.FundraisingProject.Repositories;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public interface DonorRepo extends CrudRepository{
+import com.codingdojo.FundraisingProject.Models.Donor;
 
+@Repository
+public interface DonorRepo extends CrudRepository<Donor, Long>{
+	List<Donor> findAll();
 }
