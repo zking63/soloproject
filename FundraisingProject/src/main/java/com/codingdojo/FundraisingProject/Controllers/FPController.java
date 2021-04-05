2ecf345ddd5a6d689cb9a99fc449cbeb31062784
zking63 <zoemking63@gmail.com>
2021-04-05 09:14:49 -0400

    loginreg added to controller

--- FundraisingProject/src/main/java/com/codingdojo/FundraisingProject/Controllers/FPController.java
+++ FundraisingProject/src/main/java/com/codingdojo/FundraisingProject/Controllers/FPController.java
@@ -1,10 +1,19 @@
 package com.codingdojo.FundraisingProject.Controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.codingdojo.FundraisingProject.Models.User;
+import com.codingdojo.FundraisingProject.Services.UserService;
 
 @Controller
 public class FPController {
-	public String index() {
-		return "hello world";
+	@Autowired
+	private UserService uservice;
+	@RequestMapping("/")
+	public String index(@ModelAttribute("user")User user) {
+		return "loginreg.jsp";
 	}
 }
