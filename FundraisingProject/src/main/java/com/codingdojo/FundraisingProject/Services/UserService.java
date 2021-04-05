63a47264ca9956050bdbc3e29d7db5ed24fb954e
zking63 <zoemking63@gmail.com>
2021-04-05 09:07:28 -0400

    register user added to service

--- FundraisingProject/src/main/java/com/codingdojo/FundraisingProject/Services/UserService.java
+++ FundraisingProject/src/main/java/com/codingdojo/FundraisingProject/Services/UserService.java
@@ -1,12 +1,21 @@
 package com.codingdojo.FundraisingProject.Services;
 
+import org.mindrot.jbcrypt.BCrypt;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.codingdojo.FundraisingProject.Models.User;
 import com.codingdojo.FundraisingProject.Repositories.UserRepo;
 
 @Service
 public class UserService {
 	@Autowired
 	private UserRepo urepo;
+	
+	//register user
+	public User registerUser(User user) {
+		String hashed = BCrypt.hashpw(user.getPassword(), BCrypt.gensalt());
+		user.setPassword(hashed);
+		return urepo.save(user);
+	}
 }
