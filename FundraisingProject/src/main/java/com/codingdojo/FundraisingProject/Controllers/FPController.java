f6d2b45d0257dac837427916364217608574bc5c
zking63 <zoemking63@gmail.com>
2021-04-05 09:15:25 -0400

    register user added to controller

--- FundraisingProject/src/main/java/com/codingdojo/FundraisingProject/Controllers/FPController.java
+++ FundraisingProject/src/main/java/com/codingdojo/FundraisingProject/Controllers/FPController.java
@@ -1,9 +1,14 @@
 package com.codingdojo.FundraisingProject.Controllers;
 
+import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.codingdojo.FundraisingProject.Models.User;
 import com.codingdojo.FundraisingProject.Services.UserService;
@@ -15,5 +20,15 @@ public class FPController {
 	@RequestMapping("/")
 	public String index(@ModelAttribute("user")User user) {
 		return "loginreg.jsp";
+	}
+	@RequestMapping(value="/", method=RequestMethod.POST)
+	public String registerUser(@Valid @ModelAttribute("user") User user, BindingResult result, HttpSession session) {
+		//uvalidation.validate(user, result);
+		if (result.hasErrors()) {
+			return "loginreg.jsp";
+		}
+		User newUser = uservice.registerUser(user);
+		session.setAttribute("user_id", newUser.getId());
+		return "redirect:/ideas";
 	}
 }
