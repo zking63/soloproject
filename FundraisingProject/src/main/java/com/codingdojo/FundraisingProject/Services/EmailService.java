8d9538e436c13993385e15a4e8591125a6703408
zking63 <zoemking63@gmail.com>
2021-04-07 07:51:01 -0400

    create email added to service

--- FundraisingProject/src/main/java/com/codingdojo/FundraisingProject/Services/EmailService.java
+++ FundraisingProject/src/main/java/com/codingdojo/FundraisingProject/Services/EmailService.java
@@ -3,10 +3,15 @@ package com.codingdojo.FundraisingProject.Services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.codingdojo.FundraisingProject.Models.Emails;
 import com.codingdojo.FundraisingProject.Repositories.EmailRepo;
 
 @Service
 public class EmailService {
 	@Autowired
 	private EmailRepo erepo;
+	
+	public Emails createEmail(Emails email) {
+		return erepo.save(email);
+	}
 }
