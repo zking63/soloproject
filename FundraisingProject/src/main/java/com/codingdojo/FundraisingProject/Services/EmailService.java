bef6d4d37fb23badcfe3040ebbaae664404d44b5
zking63 <zoemking63@gmail.com>
2021-04-07 07:49:17 -0400

    email service created

---
+++ FundraisingProject/src/main/java/com/codingdojo/FundraisingProject/Services/EmailService.java
@@ -0,0 +1,12 @@
+package com.codingdojo.FundraisingProject.Services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.codingdojo.FundraisingProject.Repositories.EmailRepo;
+
+@Service
+public class EmailService {
+	@Autowired
+	private EmailRepo erepo;
+}
