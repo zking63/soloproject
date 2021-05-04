e0c46e33a900b34e8bed4c432244ab230eb3601c
zking63 <zoemking63@gmail.com>
2021-05-04 12:27:03 -0400

    donor sum range added to functions

--- FundraisingProject/src/main/java/com/codingdojo/FundraisingProject/Repositories/DonorRepo.java
+++ FundraisingProject/src/main/java/com/codingdojo/FundraisingProject/Repositories/DonorRepo.java
@@ -50,13 +50,14 @@ public interface DonorRepo extends CrudRepository<Donor, Long>{
 	List<Donor> findByDonorsumByDesc(@Param("startdate") @DateTimeFormat(pattern ="yyyy-MM-dd") String startdate, 
 			@Param("enddate") @DateTimeFormat(pattern ="yyyy-MM-dd") String enddate);
 	
+	//sum within rage functions
+	@Query(value = "SELECT SUM(donations.amount) FROM donors LEFT JOIN donations ON donations.donor_id = donors.id WHERE donors.id = :donorid AND donations.dondate >= :startdate and donations.dondate <= :enddate AND donors.mostrecent_date >= :startdate and donors.mostrecent_date <= :enddate", nativeQuery = true)
+	Integer donorsumRange(@Param("donorid") Long id, @Param("startdate") @DateTimeFormat(pattern ="yyyy-MM-dd") String startdate, 
+			@Param("enddate") @DateTimeFormat(pattern ="yyyy-MM-dd") String enddate);
+	
 	//donation count functions
 	@Query(value = "SELECT COUNT(DISTINCT donations.id) FROM donors LEFT JOIN donations ON donations.donor_id = donors.id WHERE donors.id = :donorid", nativeQuery = true)
 	Integer donordoncount(@Param("donorid") Long id);
-	
-	@Query(value = "SELECT COUNT(DISTINCT donations.id) FROM donors LEFT JOIN donations ON donations.donor_id = donors.id WHERE donors.id = :donorid AND donations.dondate >= :startdate and donations.dondate <= :enddate AND donors.mostrecent_date >= :startdate and donors.mostrecent_date <= :enddate", nativeQuery = true)
-	Integer donordoncountRange(@Param("donorid") Long id, @Param("startdate") @DateTimeFormat(pattern ="yyyy-MM-dd") String startdate, 
-			@Param("enddate") @DateTimeFormat(pattern ="yyyy-MM-dd") String enddate);
 	
 	@Query(value = "SELECT * FROM donors LEFT JOIN data_donors ON donors.id = data_donors.donor_id where donors.mostrecent_date >= :startdate and donors.mostrecent_date <= :enddate ORDER BY data_donors.donor_contributioncount DESC", nativeQuery = true)
 	List<Donor> findByContributionCountByDesc(@Param("startdate") @DateTimeFormat(pattern ="yyyy-MM-dd") String startdate, 
@@ -64,6 +65,11 @@ public interface DonorRepo extends CrudRepository<Donor, Long>{
 	
 	@Query(value = "SELECT * FROM donors LEFT JOIN data_donors ON donors.id = data_donors.donor_id where donors.mostrecent_date >= :startdate and donors.mostrecent_date <= :enddate ORDER BY data_donors.donor_contributioncount ASC", nativeQuery = true)
 	List<Donor> findByContributionCountByAsc(@Param("startdate") @DateTimeFormat(pattern ="yyyy-MM-dd") String startdate, 
+			@Param("enddate") @DateTimeFormat(pattern ="yyyy-MM-dd") String enddate);
+	
+	//count within range function 
+	@Query(value = "SELECT COUNT(DISTINCT donations.id) FROM donors LEFT JOIN donations ON donations.donor_id = donors.id WHERE donors.id = :donorid AND donations.dondate >= :startdate and donations.dondate <= :enddate AND donors.mostrecent_date >= :startdate and donors.mostrecent_date <= :enddate", nativeQuery = true)
+	Integer donordoncountRange(@Param("donorid") Long id, @Param("startdate") @DateTimeFormat(pattern ="yyyy-MM-dd") String startdate, 
 			@Param("enddate") @DateTimeFormat(pattern ="yyyy-MM-dd") String enddate);
 	
 	//most recent donation function
